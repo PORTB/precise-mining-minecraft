6d26d6a28c64043cd3e17f318dc293cf124c59ba
PORTB <34615701+PORTB@users.noreply.github.com>
2021-03-30 20:35:28 +1100

    Update ClientMixins.java

--- src/main/java/thatguy/mod/miningspeed2/mixin/ClientMixins.java
+++ src/main/java/thatguy/mod/miningspeed2/mixin/ClientMixins.java
@@ -1,6 +1,9 @@
 package thatguy.mod.miningspeed2.mixin;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.PlayerControllerMP;
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
@@ -12,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.InjectionPoint;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import thatguy.mod.miningspeed2.CustomPlayerController;
 
 import static thatguy.mod.miningspeed2.MiningSpeed.customPlayerController;
 import static thatguy.mod.miningspeed2.MiningSpeed.minecraft;
@@ -20,8 +24,8 @@ import static thatguy.mod.miningspeed2.MiningSpeed.minecraft;
 public class ClientMixins
 {
     //@Inject(method = {"sendClickBlockToController"}, at={@At("invoke")}, cancellable = true)
-    @Redirect(method = {"sendClickBlockToController"}, at = @At("INVOKE"))
-    public void sendClickBlockToController(boolean leftClick)
+    @Redirect(method = "processKeyBinds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;sendClickBlockToController(Z)V"))
+    public void sendClickBlockToController(Minecraft minecraft, boolean leftClick)
     {
         if (!leftClick)
         {
@@ -47,5 +51,11 @@ public class ClientMixins
                 minecraft.playerController.resetBlockRemoving();
             }
         }
+    }
+
+    @Redirect(method = "clickMouse", at=@At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/PlayerControllerMP;clickBlock(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumFacing;)V"))
+    public void clickBlock(PlayerControllerMP controller, BlockPos location, EnumFacing facing)
+    {
+        customPlayerController.clickBlock(location, facing);
     }
 }
