1a85d33c134fbe7178b674624159d35702ce0ffa
PORTB <34615701+PORTB@users.noreply.github.com>
2021-03-30 21:14:45 +1100

    INITIAL IMPLEMENTATION COMPLETE!!!!!!
    HOLY SHIT IT ACTALLY WORKS

--- src/main/java/thatguy/mod/miningspeed2/mixin/SendClickBlockToControllerMixin.java
+++ src/main/java/thatguy/mod/miningspeed2/mixin/SendClickBlockToControllerMixin.java
@@ -9,6 +9,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,11 +26,51 @@ import static thatguy.mod.miningspeed2.MiningSpeed.minecraft;
 @Mixin({Minecraft.class})
 public class SendClickBlockToControllerMixin
 {
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Redirect(method = "processKeyBinds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;clickMouse()V"))
     public void clickMouse(Minecraft minecraft)
     {
-        minecraft.player.sendMessage(new TextComponentString("ckcok and bakkl"));
+        if (minecraft.leftClickCounter <= 0)
+        {
+            if (minecraft.objectMouseOver == null)
+            {
+                LOGGER.error("Null returned as 'hitResult', this shouldn't happen!");
+
+                if (minecraft.playerController.isNotCreative())
+                {
+                    minecraft.leftClickCounter = 10;
+                }
+            } else if (!minecraft.player.isRowingBoat())
+            {
+                switch (minecraft.objectMouseOver.typeOfHit)
+                {
+                    case ENTITY:
+                        minecraft.playerController.attackEntity(minecraft.player, minecraft.objectMouseOver.entityHit);
+                        break;
+                    case BLOCK:
+                        BlockPos blockpos = minecraft.objectMouseOver.getBlockPos();
+
+                        if (!minecraft.world.isAirBlock(blockpos))
+                        {
+                            customPlayerController.clickBlock(blockpos, minecraft.objectMouseOver.sideHit);
+                            break;
+                        }
+
+                    case MISS:
+
+                        if (minecraft.playerController.isNotCreative())
+                        {
+                            minecraft.leftClickCounter = 10;
+                        }
+
+                        minecraft.player.resetCooldown();
+                        net.minecraftforge.common.ForgeHooks.onEmptyLeftClick(minecraft.player);
+                }
+
+                minecraft.player.swingArm(EnumHand.MAIN_HAND);
+            }
+        }
     }
 
     @Redirect(method = "processKeyBinds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;sendClickBlockToController(Z)V"))
@@ -52,8 +94,7 @@ public class SendClickBlockToControllerMixin
                     minecraft.effectRenderer.addBlockHitEffects(blockpos, minecraft.objectMouseOver);
                     minecraft.player.swingArm(EnumHand.MAIN_HAND);
                 }
-            }
-            else
+            } else
             {
                 minecraft.playerController.resetBlockRemoving();
             }
