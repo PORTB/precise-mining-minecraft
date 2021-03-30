9464923c1f016c7932f7330b13a5b3837488867a
PORTB <34615701+PORTB@users.noreply.github.com>
2021-03-30 18:24:24 +1100

    Implemented preventing insta-breaking blocks attempt 1

--- src/main/java/thatguy/mod/miningspeed2/proxy/ClientProxy.java
+++ src/main/java/thatguy/mod/miningspeed2/proxy/ClientProxy.java
@@ -3,11 +3,22 @@ package thatguy.mod.miningspeed2.proxy;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import static thatguy.mod.miningspeed2.MiningSpeed.customPlayerController;
+import static thatguy.mod.miningspeed2.MiningSpeed.minecraft;
+
 public class ClientProxy extends CommonProxy
 {
     @SubscribeEvent
     public void clientTickEvent(TickEvent.ClientTickEvent event)
     {
+        resetHasBrokenBlockIfMouseNotPressed();
+    }
 
+    private void resetHasBrokenBlockIfMouseNotPressed()
+    {
+        if(minecraft.gameSettings.keyBindAttack.isKeyDown())
+        {
+            customPlayerController.hasBrokenBlock = false;
+        }
     }
 }
