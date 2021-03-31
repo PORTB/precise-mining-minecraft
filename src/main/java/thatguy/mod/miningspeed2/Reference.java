3ee68fca6f0a8c20134a69d7919f6709b1386523
PORTB <34615701+PORTB@users.noreply.github.com>
2021-03-31 19:20:16 +1100

    Use the correct key code

--- src/main/java/thatguy/mod/miningspeed2/Reference.java
+++ src/main/java/thatguy/mod/miningspeed2/Reference.java
@@ -6,9 +6,9 @@ public class Reference
 {
     public static final String MOD_ID = "miningspeed2";
     public static final String NAME = "Precision Mining";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
 
     public final static String MINING_CONTROL_ENABLED_TAG = "mining_control_enabled";
 
-    public static KeyBinding toggleSpeedControlKey = new KeyBinding("Toggle Mining Speed Control", 92 /*backslash*/, "Mining Control");
+    public static KeyBinding toggleSpeedControlKey = new KeyBinding("Toggle Mining Speed Control", 43 /*backslash*/, "Mining Control");
 }
