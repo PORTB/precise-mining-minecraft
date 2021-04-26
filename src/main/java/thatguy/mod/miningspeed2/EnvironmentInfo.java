a16ece2b7f0d1ed1b0ef589d2dc2c86e0eca6133
PORTB <34615701+PORTB@users.noreply.github.com>
2021-04-26 20:31:30 +1000

    Implemented detecting if mekanism is installed

---
+++ src/main/java/thatguy/mod/miningspeed2/EnvironmentInfo.java
@@ -0,0 +1,32 @@
+package thatguy.mod.miningspeed2;
+
+import net.minecraft.launchwrapper.Launch;
+import java.util.Map;
+
+public class EnvironmentInfo
+{
+    public static final String MEKANISM_ID = "mekanism";
+    private static Boolean _isMekanismInstalled = null;
+
+    @SuppressWarnings("unchecked")
+    public static boolean isMekanismInstalled()
+    {
+        if (_isMekanismInstalled == null)
+        {
+            Map<String, Map<String, String>> modList = (Map<String, Map<String, String>>) Launch.blackboard.get("modList");
+
+            for (Map.Entry<String, Map<String, String>> mod : modList.entrySet())
+            {
+                if (mod.getValue().get("id").equals(MEKANISM_ID))
+                {
+                    _isMekanismInstalled = true;
+                    return true;
+                }
+            }
+
+            _isMekanismInstalled = false;
+        }
+
+        return _isMekanismInstalled;
+    }
+}
