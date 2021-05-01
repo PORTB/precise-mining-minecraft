ed8726b5fe066ecd5820075ede640c8c8445943b
PORTB <34615701+PORTB@users.noreply.github.com>
2021-05-01 18:45:04 +1000

    Change version

--- src/main/java/thatguy/mod/miningspeed2/Reference.java
+++ src/main/java/thatguy/mod/miningspeed2/Reference.java
@@ -6,9 +6,10 @@ public class Reference
 {
     public static final String MOD_ID = "miningspeed2";
     public static final String NAME = "Precision Mining";
-    public static final String VERSION = "1.6";
+    public static final String VERSION = "1.7";
 
     public final static String MINING_CONTROL_ENABLED_TAG = "mining_control_enabled";
 
-    public static KeyBinding toggleSpeedControlKey = new KeyBinding("Toggle Mining Speed Control", 43 /*backslash*/, "Mining Control");
+    public final static String CLIENT_PROXY = "thatguy.mod.miningspeed2.proxy.ClientProxy";
+    public final static String COMMON_PROXY = "thatguy.mod.miningspeed2.proxy.CommonProxy";
 }
